38528e84005b9bfacba2435b88d7d515451a0478
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-11 11:14:34 -0700

    ignored node modules

---
+++ functions.test.js
@@ -0,0 +1,14 @@
+const { TestWatcher } = require('jest')
+const myFunctions = require('./functions')
+
+test('Return two test', () => {
+    expect(myFunctions.returnTwo()).toBe(2)
+})
+
+test('Greeting test', () => {
+    expect(myFunctions.greeting('Kelsey').toBe('Hello, Kelsey.'))
+})
+
+test('Add test', () => {
+    expect(myFunctions.add(5, 9)).toBe(14)
+})
